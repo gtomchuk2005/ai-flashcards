72e6de47527b019fd02a7ec7fdc2106e9a57d66b
gtomchuk2005 <gtomchuk2005@gmail.com>
2024-08-16 18:03:47 -0400

    Protect root route with auth

--- app/page.js
+++ app/page.js
@@ -1,5 +1,36 @@
+"use client";
+import { auth } from '@/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useRouter, usePathname } from "next/navigation";
+import { useState, useEffect } from 'react';
+
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const pathname = usePathname();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+          if (!user) {
+            console.log('user signed out');
+
+            if (pathname !== '/signup') {
+              router.push('/login');
+            }
+            else {
+              router.push('/signup'); 
+            }
+          }
+          setLoading(false)
+        })
+    })
+
+    if (loading) {
+      return <div>Loading...</div>
+    }
+
   return (
     <h1>hi</h1>
   );
+
 }
