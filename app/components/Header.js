2ef890583635e555784caf37f909e1a408dfbdbd
gtomchuk2005 <gtomchuk2005@gmail.com>
2024-08-16 18:26:44 -0400

    Fix Header component after merge conflict

--- app/components/Header.js
+++ app/components/Header.js
@@ -24,21 +24,17 @@ export default function Header() {
                         <Link href='/' color="black" underline="none">FlashcardsAI</Link>
                     </Typography>
 
-
-                    <Box ml="auto" mr={5}>
-                        <AccountCircleIcon sx={{ color: "black", marginRight: "10px", fontSize: "25px"}} />
+                    <Box display="flex" alignItems="center" ml="auto" mr={5}>
+                        <Button p={5} sx={{ backgroundColor: "#982bff", marginRight: "20px", borderRadius: "25px", paddingX: "20px", transition: "transform 0.2s ease-in-out", '&:hover': { backgroundColor: "#982bff", transform: "scale(1.1)"} }} >
+                            <Typography color="white" fontWeight="bolder">
+                                Login
+                            </Typography>
+                        </Button>
                         <MenuIcon onClick={handleOpen} sx={{ color: "black", fontSize: "25px" }} />
                     </Box>
 
-                <Box display="flex" alignItems="center" ml="auto" mr={5}>
-                    <Button p={5} sx={{ backgroundColor: "#982bff", marginRight: "20px", borderRadius: "25px", paddingX: "20px", transition: "transform 0.2s ease-in-out", '&:hover': { backgroundColor: "#982bff", transform: "scale(1.1)"} }} >
-                        <Typography color="white" fontWeight="bolder">
-                            Login
-                        </Typography>
-                    </Button>
-                    <MenuIcon onClick={handleOpen} sx={{ color: "black", fontSize: "25px" }} />
-
                 </Box>
+                
             </AppBar>
 
             <Drawer anchor="right" open={isOpen} onClose={handleClose} PaperProps={{ sx: { border: "2px solid #982bff", borderRadius: "20px", margin: "10px"}}}>
