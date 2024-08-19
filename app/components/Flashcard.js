3fe7deb2e6a108a1280fc5a9186c82f25a55bfad
gtomchuk2005 <gtomchuk2005@gmail.com>
2024-08-18 23:26:46 -0400

    Fix styling of flashcard

--- app/components/Flashcard.js
+++ app/components/Flashcard.js
@@ -11,9 +11,9 @@
 
         return(
             <Box>
-                <Box width="1000px" height="300px" sx={{ position: "relative", marginTop: "50px" }}>
+                <Box width="1000px" height="300px" sx={{ position: "relative", marginTop: "30px" }}>
                     <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" width="100%" height="100%" sx={{ position: "absolute", transition: "background-color 0.3s ease-in-out", backgroundColor: turnCard ? "#50f163" : "#982bff", borderRadius: "50px" }}>
-                        <Typography fontFamily="Radio Canada Big" sx={{ transition: "color 0.5s ease-in-out", color: turnCard ? "#000000" : "white" }}>
+                        <Typography fontFamily="Radio Canada Big" sx={{ textAlign: 'center', transition: "color 0.5s ease-in-out", color: turnCard ? "#000000" : "white", padding: '10px'}}>
                             {turnCard ? (answer) : (question) }
                         </Typography>
                         <Button variant="contained" onClick={handleTurnCard} sx={{ position: "absolute", bottom: "20px", left: "50%", transform: "translateX(-50%)" }}>
